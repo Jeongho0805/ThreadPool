1d8014e205de80e7245aebd965454845121cc678
jeongho <pjhpjh0805@naver.com>
2023-12-26 22:35:21 +0900

    2023-12-26

--- src/main/java/threadpool/MyThreadPool.java
+++ src/main/java/threadpool/MyThreadPool.java
@@ -1,22 +1,28 @@
 package threadpool;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executor;
 import java.util.concurrent.LinkedTransferQueue;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MyThreadPool implements Executor {
 
     private static final Runnable SHUTDOWN_TASK = () -> {};
 
     private final BlockingQueue<Runnable> queue = new LinkedTransferQueue<>();
-    private final Thread[] threads;
     private final AtomicBoolean started = new AtomicBoolean();
     private final AtomicBoolean shutdown = new AtomicBoolean();
+    private final AtomicInteger numActiveThreads = new AtomicInteger();
+    private final Set<Thread> threads = new HashSet<>();
+    private final Lock threadsLock = new ReentrantLock();
 
     public MyThreadPool(int numThreads) {
-        threads = new Thread[numThreads];
         for (int i = 0; i < numThreads; i++) {
             threads[i] = new Thread(() -> {
                 for (;;) {
@@ -53,9 +59,26 @@ public class MyThreadPool implements Executor {
         }
 
         queue.add(command);
+        addThreadIfNecessary();
 
         if (shutdown.get()) {
             queue.remove(command);
+        }
+    }
+
+    // note 왜 ActiveThreads 갯수가 실제 threads 갯수 보다 많을 때 쓰레드가 추가되어야 하는걸까?
+    private void addThreadIfNecessary() {
+        if (numActiveThreads.get() >=  threads.size()) {
+            threadsLock.lock();
+            try {
+                if (numActiveThreads.get() >= threads.size()) {
+                    final Thread newThread = new Thread();
+                    threads.add(newThread);
+                    newThread.start();
+                }
+            } finally {
+                threadsLock.unlock();
+            }
         }
     }
 
@@ -65,9 +88,6 @@ public class MyThreadPool implements Executor {
                 queue.add(SHUTDOWN_TASK);
             }
         }
-
-
-
 
         for (Thread thread : threads) {
             do {
